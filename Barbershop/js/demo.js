45089320217e080c91751f104b1d498a5ff63fbd
joedonny63 <101237380+joedonny63@users.noreply.github.com>
2022-08-13 06:39:02 -0400

    Add files via upload

---
+++ Barbershop/js/demo.js
@@ -0,0 +1,123 @@
+(() => {
+    function t(o) {
+        return t = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (t) {
+            return typeof t
+        } : function (t) {
+            return t && "function" == typeof Symbol && t.constructor === Symbol && t !== Symbol.prototype ? "symbol" : typeof t
+        }, t(o)
+    }! function () {
+        function o(n) {
+            return (o = "function" == typeof Symbol && "symbol" == t(Symbol.iterator) ? function (o) {
+                return t(o)
+            } : function (o) {
+                return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : t(o)
+            })(n)
+        }! function () {
+            function t(n) {
+                return (t = "function" == typeof Symbol && "symbol" == o(Symbol.iterator) ? function (t) {
+                    return o(t)
+                } : function (t) {
+                    return t && "function" == typeof Symbol && t.constructor === Symbol && t !== Symbol.prototype ? "symbol" : o(t)
+                })(n)
+            }! function () {
+                function o(n) {
+                    return (o = "function" == typeof Symbol && "symbol" == t(Symbol.iterator) ? function (o) {
+                        return t(o)
+                    } : function (o) {
+                        return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : t(o)
+                    })(n)
+                }! function () {
+                    function t(n) {
+                        return (t = "function" == typeof Symbol && "symbol" == o(Symbol.iterator) ? function (t) {
+                            return o(t)
+                        } : function (t) {
+                            return t && "function" == typeof Symbol && t.constructor === Symbol && t !== Symbol.prototype ? "symbol" : o(t)
+                        })(n)
+                    }! function () {
+                        function o(n) {
+                            return (o = "function" == typeof Symbol && "symbol" == t(Symbol.iterator) ? function (o) {
+                                return t(o)
+                            } : function (o) {
+                                return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : t(o)
+                            })(n)
+                        }! function () {
+                            function t(n) {
+                                return (t = "function" == typeof Symbol && "symbol" == o(Symbol.iterator) ? function (t) {
+                                    return o(t)
+                                } : function (t) {
+                                    return t && "function" == typeof Symbol && t.constructor === Symbol && t !== Symbol.prototype ? "symbol" : o(t)
+                                })(n)
+                            }! function () {
+                                "use strict";
+
+                                function o(n) {
+                                    return (o = "function" == typeof Symbol && "symbol" == t(Symbol.iterator) ? function (o) {
+                                        return t(o)
+                                    } : function (o) {
+                                        return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : t(o)
+                                    })(n)
+                                }
+                                document.addEventListener("DOMContentLoaded", (function () {
+                                    ! function (t) {
+                                        if ("object" === o(t)) {
+                                            var n = t.delay,
+                                                e = t.slugPrefix,
+                                                i = t.slugSuffix,
+                                                r = t.msg,
+                                                c = document.createElement("div"),
+                                                l = document.createElement("a"),
+                                                f = Date.now(),
+                                                a = 0,
+                                                s = 0,
+                                                u = 0,
+                                                m = window.innerHeight - 108;
+                                            setTimeout((function () {
+                                                var t;
+                                                c.classList.add("mdp-float"), c.classList.add("mdp-float-init"), l.classList.add("mdp-float-btn"), l.href = (t = i.length > 0 ? "-".concat(i) : "", "https://1.envato.market/" + p() + t), l.target = "_blank", l.innerHTML = '<svg height="512" viewBox="0 0 512 512" width="512" xmlns="http://www.w3.org/2000/svg"><path d="m506.887 114.74c-3.979-5.097-10.086-8.076-16.553-8.076h-399.808l-5.943-66.207c-.972-10.827-10.046-19.123-20.916-19.123h-42.667c-11.598 0-21 9.402-21 21s9.402 21 21 21h23.468l23.018 256.439c.005.302-.01.599.007.903.047.806.152 1.594.286 2.37l.842 9.376c.016.177.034.354.055.529 2.552 22.11 13.851 41.267 30.19 54.21-8.466 10.812-13.532 24.407-13.532 39.172 0 35.106 28.561 63.667 63.666 63.667 35.106 0 63.667-28.561 63.667-63.667 0-7.605-1.345-14.9-3.801-21.667h114.936c-2.457 6.767-3.801 14.062-3.801 21.667 0 35.106 28.561 63.667 63.667 63.667s63.667-28.561 63.667-63.667-28.561-63.667-63.667-63.667h-234.526c-15.952 0-29.853-9.624-35.853-23.646l335.608-19.724c9.162-.538 16.914-6.966 19.141-15.87l42.67-170.67c1.567-6.272.158-12.918-3.821-18.016z"/></svg>', l.rel = "noreferrer noopener", c.appendChild(l), c.style.top = "".concat(window.scrollY + window.innerHeight - 108, "px"), document.body.appendChild(c), setTimeout((function () {
+                                                    c.classList.remove("mdp-float-init"), c.classList.remove("mdp-float-inertia")
+                                                }), 1001), "iframe" === r && window.self !== window.top && function (t, o, n, e) {
+                                                    var i = document.createElement("div");
+                                                    if (i.classList.add("mdp-float-popup"), "".length > 0) {
+                                                        var r = document.createElement("h4");
+                                                        r.classList.add("mdp-float-h"), r.innerHTML = "", i.appendChild(r)
+                                                    }
+                                                    var l = document.createElement("p");
+                                                    l.classList.add("mdp-float-msg"), l.innerHTML = o, i.appendChild(l);
+                                                    var f = document.createElement("a");
+                                                    f.classList.add("mdp-float-action"), f.href = e, f.target = "_blank", f.rel = "noreferrer noopener", f.innerHTML = "<span>".concat("Open without iframe", "</span>"), i.appendChild(f), setTimeout((function () {
+                                                        c.appendChild(i)
+                                                    }), 1500)
+                                                }(0, "Some features of ".concat(p().charAt(0).toUpperCase() + p().slice(1), " may not work correctly inside an iframe."), 0, window.location.href)
+                                            }), n), setInterval((function () {
+                                                var t = Date.now() - f;
+                                                c.classList.remove("mdp-float-slow"), c.classList.remove("mdp-float-inertia"),
+                                                    /*!catMouseOver &&*/
+                                                    t - a > 2e3 && t - a < 2999 && (m = t > n + 2021 && Math.abs(s - u) < 500 ? u + window.innerHeight - 2 * c.offsetHeight : u + window.innerHeight > c.offsetTop ? u + window.innerHeight - 1.5 * c.offsetHeight : u + window.innerHeight - 2.5 * c.offsetHeight, c.style.top = "".concat(m, "px"), setTimeout((function () {
+                                                        c.classList.add("mdp-float-inertia"), c.style.top = "".concat(u + window.innerHeight - 2 * c.offsetHeight, "px")
+                                                    }), 751), s = u)
+                                            }), 2e3), window.addEventListener("scroll", (function (t) {
+                                                a = t.timeStamp, u = window.scrollY, c.classList.add("mdp-float-slow"), m = u + window.innerHeight - 2 * c.offsetHeight, c.style.top = "".concat(m, "px")
+                                            })), c.addEventListener("mouseenter", (function () {
+                                                c.style.top = "".concat(u + c.getBoundingClientRect().top, "px")
+                                            })), c.addEventListener("mouseout", (function () {}))
+                                        }
+
+                                        function p() {
+                                            var t = "barbercrop";
+                                            return "start" === e && (t = window.location.hostname.split(".")[0]), "end" === e && (t = window.location.pathname.split("/")[window.location.pathname.split("/").length - 1]), t
+                                        }
+                                    }({
+                                        delay: 3500,
+                                        slugPrefix: "KJKeeton",
+                                        slugSuffix: "html",
+                                        msg: ""
+                                    })
+                                }))
+                            }()
+                        }()
+                    }()
+                }()
+            }()
+        }()
+    }()
+})();
